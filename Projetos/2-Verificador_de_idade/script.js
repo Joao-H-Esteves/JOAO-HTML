e952141f9ec9148c6d15c8b19d1a02338ff271dd
JOAO HENRIQUE ESTEVES <joaohesteves@msn.com>
2022-02-03 12:49:49 -0300

    alteração para padrao w3c

--- Projetos/2-Verificador_de_idade/script.js
+++ Projetos/2-Verificador_de_idade/script.js
@@ -31,56 +31,56 @@ function verific() {
             genero = 'Homem'
             if (idade >=0 && idade <=6) {
                 //CRIANÇA PEQUENA
-                img.setAttribute('src','crianca_peq_masc.png');
+                img.setAttribute('src','img/crianca_peq_masc.png');
                 
                 }else if (idade >=7 && idade <= 12) {
                     // CRIANÇA (GRANDE)
-                    img.setAttribute('src','crianca_grande_masc.png');
+                    img.setAttribute('src','img/crianca_grande_masc.png');
                     
                 }else if (idade <= 18) { // POR LÓGICA COMO ATE 12 É CRIANÇA, ENTÃ0 QUALQUER IDADE QUE FOR MENOR OU IGUAL A 18 JA SE ENCAIXA AQUI, POIS O PARAMETRO ACIMA JA ESTIPULA O QUE ACONTECE AO 12 OU MENOS.
 
                     // ADOLECENTE
-                    img.setAttribute('src','adolec_masc.png');
+                    img.setAttribute('src','img/adolec_masc.png');
 
                 }else if (idade <= 30){
                     // ADULTO JOVEM
-                    img.setAttribute('src', 'adulto_jovem_masc.png');
+                    img.setAttribute('src', 'img/adulto_jovem_masc.png');
                    
                 }else if (idade <= 59){
                     // ADULTO MEDIO
-                    img.setAttribute('src', 'adulto_medio_masc.png');
+                    img.setAttribute('src', 'img/adulto_medio_masc.png');
 
                 }else if (idade > 59){
                     // iDOSO
-                    img.setAttribute('src','idoso_masc.png');
+                    img.setAttribute('src','img/idoso_masc.png');
                 }
 
         }else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >=0 && idade <=6) {
                 //CRIANÇA PEQUENA
-                img.setAttribute('src','crianca_peq_fem.png');
+                img.setAttribute('src','img/crianca_peq_fem.png');
 
                 }else if (idade >=7 && idade <= 12) {
                     // CRIANÇA (GRANDE)
-                    img.setAttribute('src','crianca_grande_fem.png');
+                    img.setAttribute('src','img/crianca_grande_fem.png');
 
                 }else if (idade <= 18) { // POR LÓGICA COMO ATE 12 É CRIANÇA, ENTÃ0 QUALQUER IDADE QUE FOR MENOR OU IGUAL A 18 JA SE ENCAIXA AQUI, POIS O PARAMETRO ACIMA JA ESTIPULA O QUE ACONTECE AO 12 OU MENOS.
 
                     // ADOLECENTE
-                    img.setAttribute('src','adolec_fem.png');
+                    img.setAttribute('src','img/adolec_fem.png');
 
                 }else if (idade <= 30){
                     // ADULTO JOVEM
-                    img.setAttribute('src', 'adulto_jovem_fem.png');
+                    img.setAttribute('src', 'img/adulto_jovem_fem.png');
 
                 }else if (idade <= 59){
                     // ADULTO MEDIO
-                    img.setAttribute('src', 'adulto_medio_fem.png');
+                    img.setAttribute('src', 'img/adulto_medio_fem.png');
 
                 }else if (idade > 59){
                     // iDOSO
-                    img.setAttribute('src','idoso_fem.png');
+                    img.setAttribute('src','img/idoso_fem.png');
                 }
         }
         res.style.textAlign = 'center' 
